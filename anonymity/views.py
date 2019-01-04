7e6f618ee07fb657914ff186c9188448fbe3d4df
Администратор <@MACHINE-JD2FT0C>
2019-01-04 23:21:44 +0400

    ТЫЦ-ТЫЦ

--- anonymity/views.py
+++ anonymity/views.py
@@ -2,12 +2,7 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
-from flask import render_template
-from flask import request
-from anonymity import app
-from anonymity import utils
-from anonymity import portscan
+
 
 checklist = {"lang" : True,
              "header" : True,
