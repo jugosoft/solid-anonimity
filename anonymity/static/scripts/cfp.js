f4a5c8e8e1107e785d19458ef4cfee1d14398f8a
Администратор <@MACHINE-JD2FT0C>
2019-01-08 10:28:40 +0400

    canvas fingerprinting is added

---
+++ anonymity/static/scripts/cfp.js
@@ -0,0 +1,27 @@
+String.prototype.hashCode = function () {
+    var hash = 0, i, chr, len;
+
+    if (this.length == 0)
+        return hash;
+
+    for (i = 0, len = this.length; i < len; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0;
+    }
+
+    return hash;
+};
+
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+
+//context.font = "18pt Arial";
+context.fillText("Do I know you? \ud83d\ude03", 20, 20);
+
+var dataURL = canvas.toDataURL("image/png").substr("data:image/png;base64,".length);
+//document.getElementById("cfp").innerHTML = dataURL;
+document.getElementById("cfp").innerHTML += "Hash: " + dataURL.hashCode();
+document.body.appendChild(canvas);
+//hide IT
+canvas.style.display = 'none';
--- anonymity/static/scripts/geo.js
+++ anonymity/static/scripts/geo.js
@@ -1,12 +1,11 @@
 (function geo() {
 
     var elem = document.getElementById('geo');
-    elem.innerHTML = "Does someone track your location? ";
 
     navigator.geolocation.watchPosition(
 
         function (position) {
-            elem.innerHTML += " I'm tracking you!";
+            elem.innerHTML = " I'm tracking you!";
         },
 
         function (e) {
