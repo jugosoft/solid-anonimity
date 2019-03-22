81d40ab22a328b72fbe49606e5f43ff3af070a09
Администратор <MACHINE-JD2FT0C>
2019-03-22 17:23:45 +0400

    Another one minor update...

--- anonymity/static/scripts/custom/anon_state.js
+++ anonymity/static/scripts/custom/anon_state.js
@@ -1,4 +1,4 @@
-var state = function () {
+var State = function () {
     this.dns = null;
     this.proxies = null;
     this.opened_ports = null;
@@ -13,6 +13,52 @@ var state = function () {
     this.spambases = null;
 
     this.init = function () {
-        this.dns = document.getElementById();
+        var tmp = document.getElementById('dns2');
+        if (tmp.innerText === 'DNS may be leaking.')
+            this.dns = true;        
+
+        tmp = document.getElementById('proxies');
+        if (tmp.innerText === 'True')
+            this.proxies = true;
+
+        tmp = document.getElementById('ports');
+        if (tmp.innerText.lenght >= 4)
+            this.opened_ports = true;
+
+        //our current state of anonymity level
+        this.printState();
+    }
+
+    this.printState = function () {
+        if (this.dns == true)
+            console.log('dns : 1');
+        if (this.proxies == true)
+            console.log('proxies : 1');
+        if (this.opened_ports == true)
+            console.log('opened_ports : 1');
+        if (this.js == true)
+            console.log('js : 1');
+        if (this.tor == true)
+            console.log('tor : 1');
+        if (this.flash == true)
+            console.log('flash : 1');
+        if (this.webrtc == true)
+            console.log('webrtc : 1');
+        if (this.location == true)
+            console.log('location : 1');
+        if (this.silverlight == true)
+            console.log('silverlight : 1');
+        if (this.loggedin == true)
+            console.log('loggedin : 1');
+        if (this.winmediaplayer == true)
+            console.log('winmediaplayer : 1');
+        if (this.spambases == true)
+            console.log('spambases : 1');
     }
 }
+
+var state = new State();
+state.init();
+
+
+
--- anonymity/static/scripts/custom/dnt.js
+++ anonymity/static/scripts/custom/dnt.js
@@ -2,6 +2,8 @@ function dnt() {
     var dntEnabled = navigator.doNotTrack || 1 === (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack) ? true : false;
     var element = document.getElementById('dnt');
     element.innerHTML = 'Do Not Track is enabled = ' + dntEnabled;
+    //records info about do not track settings.
+    state.doNotTrack = dntEnabled;
 }
 
 dnt();
